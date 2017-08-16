26eb6e05ca4348a760439508b84421f720457ff3
fortune chibuike <chibuikefortune@hotmail.com>
2017-08-16 10:41:11 +0100

    SF

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,14 @@ namespace :balance do
   resources :withdraw, only: [:new, :create]
   resources :deposit, only: [:new, :create]
 end
-
+authenticated :user do
+   root 'balances#index', as: "authenticated_root"
+ end
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   resources :pages, only: [:show]
   ActiveAdmin.routes(self)
   root 'welcome#index'
-  authenticated :user do
-     root 'balances#index', as: "authenticated_root"
-   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
