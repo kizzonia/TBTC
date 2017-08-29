ef03b3aba955d66c98a2c621243ff347ee94bc7f
fortune chibuike <chibuikefortune@hotmail.com>
2017-08-29 11:20:36 +0100

    email

--- app/mailers/user_deposit_mailer.rb
+++ app/mailers/user_deposit_mailer.rb
@@ -1,7 +1,5 @@
 class UserDepositMailer < ApplicationMailer
-  default :from => "Tradebitr Payment < payment@tradebitr.com"
-
-
+  default :from => "payment@tradebitr.com"
  def deposit_email(user, deposit)
    @user = user
    @deposit = deposit
